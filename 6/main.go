313c24f7c14300b95accfba58854a9ffede7491f
Soumil <soumil009@outlook.com>
2023-07-18 20:43:10 +0100

    done day 6 part 2

--- 6/main.go
+++ 6/main.go
@@ -17,7 +17,7 @@ const (
 	ON = 1
 )
 
-func main() {
+func main() { 
 	input := strings.Split(readFile("input.txt"), "\n")
 	start := time.Now()
 	partOne(input)
@@ -73,7 +73,47 @@ func partOne(input []string) {
 	}
 
 func partTwo(input []string) {
+	// store the lights state
+	lights := make(map[coordinate]int, 1_000_000)
 	
+	// parse input
+	for i := 0; i < len(input); i++ {
+		command := strings.Split(input[i], " ");
+		var start coordinate
+		var end coordinate
+		var operation string
+		if len(command) == 4 {
+			start = convertStringToCoordinates(command[1])
+			end = convertStringToCoordinates(command[3])
+			operation = command[0]
+		} else if len(command) == 5 {
+			start = convertStringToCoordinates(command[2])
+			end = convertStringToCoordinates(command[4])
+			operation = command[0] + " " + command[1]
+		}
+		for x := start.x; x <= end.x; x++ {
+			for y := start.y; y <= end.y; y++ {
+				currentCoordinate := coordinate{x, y}
+				switch operation {
+				case "toggle":
+					lights[currentCoordinate] += 2
+				case "turn on":
+					lights[currentCoordinate]++
+				case "turn off":
+					if lights[currentCoordinate] > 0 {
+						lights[currentCoordinate]--
+					}
+				default:
+					panic("unrecognised operation")
+				}
+			}
+		}
+	}
+	var totalBrightness int = 0
+	for _, v := range lights {
+		totalBrightness += v
+	}
+	fmt.Printf("Part 2: The total brightness of all the lights is %d\n", totalBrightness)
 }
 
 func filterLightsMap(m map[coordinate]int, filter func(any) bool) (map[coordinate]int) {
