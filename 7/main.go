1cc65e35ec7701c8436e2accedd453b1cda4d290
Soumil <soumil009@outlook.com>
2023-07-22 17:27:49 +0100

    fixed the program

--- 7/main.go
+++ 7/main.go
@@ -8,14 +8,11 @@ import (
 )
 
 
-type instruction struct {
-	target string
-	command []string
-	dependencies []string
-}
+type wires = map[string]uint16
 
 func main() {
 	input := strings.Split(readFile("input.txt"), "\n")
+	
 	/*input := strings.Split(`123 -> x
 	456 -> y
 	x AND y -> d
@@ -28,19 +25,99 @@ func main() {
 }
 
 func partOne(input []string) {
-	
+	wires := make(wires, len(input))
+	wiresToBeFound := input
+	for {
+		wiresToBeFoundNext := []string{}
+		for i := 0; i < len(wiresToBeFound); i++ {
+			command := strings.Split(wiresToBeFound[i], " ")
+			var succesful bool;
+			switch len(command) {
+			case 3:
+				succesful = threeLenCommand(command, &wires)
+			case 4:
+				succesful = singleInputCommand(command, &wires)
+			case 5:
+				succesful = twoInputCommand(command, &wires)
+			}
+			if !succesful {
+				wiresToBeFoundNext = append(wiresToBeFoundNext, wiresToBeFound[i])
+			}
+		}
+		_, ok := wires["a"]
+		if len(wiresToBeFoundNext) == 0 || ok {
+			break
+		}
+		wiresToBeFound = wiresToBeFoundNext
+	}
+	fmt.Printf("Part 1: The wire 'a' had the signal %d\n", wires["a"])
 }
 
-func threeLenCommand() {
-	
+func threeLenCommand(command []string, pWires *wires) bool {
+	num, err := strconv.Atoi(command[0]);
+	wires := *pWires
+	if err != nil {
+		// if there is an error that means the wire is being assigned from another wire
+		wire, ok := wires[command[0]]
+		if !ok {
+			return false
+		}
+		wires[command[2]] = wire
+		return true
+	}
+	wires[command[2]] = uint16(num)
+	return true
 }
 
-func singleInputCommand() {
-	
+func singleInputCommand(command []string, pWires *wires) bool {
+	operation := command[0]
+	wires := *pWires
+	num, err := strconv.Atoi(command[1])
+	if err != nil {
+		// it is another wire
+		wire, ok := wires[command[1]]
+		if !ok {
+			return false
+		}
+		num = int(wire)
+	}
+	switch operation {
+	case "NOT":
+		wires[command[3]] = ^uint16(num)
+	}
+	return true
 }
 
-func twoInputCommand() {
-	
+func twoInputCommand(command []string, pWires *wires) bool {
+	wires := *pWires
+	num1, err := strconv.Atoi(command[0])
+	if err != nil {
+		wire, ok := wires[command[0]]
+		if !ok {
+			return false
+		}
+		num1 = int(wire)
+	}
+	num2, err := strconv.Atoi(command[2])
+	if err != nil {
+		wire, ok := wires[command[2]]
+		if !ok {
+			return false
+		}
+		num2 = int(wire)
+	}
+	operation := command[1]
+	switch operation {
+	case "AND":
+		wires[command[len(command)-1]] = uint16(num1 & num2)
+	case "OR":
+		wires[command[len(command)-1]] = uint16(num1 | num2)
+	case "RSHIFT":
+		wires[command[len(command)-1]] = uint16(num1 >> num2)
+	case "LSHIFT":
+		wires[command[len(command)-1]] = uint16(num1 << num2)
+	}
+	return true
 }
 
 func readFile(path string) string {
